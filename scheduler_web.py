4bb6d6e84495df52b4972f05806a9f024a811a35
cych10054 <cych10054@gmail.com>
2026-06-06 20:43:52 +0800

    Create scheduler_web.py

---
+++ scheduler_web.py
@@ -0,0 +1,8 @@
+[google_sheets_key]
+type = "service_account"
+project_id = "nurse-shift-tool"
+private_key_id = "c1fe17c8cd5c4526ae4bc76c2c46fce7f9c1f019"
+private_key = "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"
+client_email = "cych10a@nurse-shift-tool.iam.gserviceaccount.com"
+client_id = "105382368489289632666"
+# (貼上您 JSON 裡面所有的內容)
