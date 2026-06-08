0da75e9b8f37cfbf51746378085a6fc58f8e31c2
cych10054 <cych10054@gmail.com>
2026-06-08 21:44:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.markdown("---")
 st.subheader("請選擇您的入口：")
 
 if st.button("📝 前往：護理師劃假系統", use_container_width=True):
-    st.switch_page("pages/1_護理師劃假.py")
+    st.switch_page("pages/1_預假.py")
 
 if st.button("🏥 前往：阿長排班系統", use_container_width=True):
-    st.switch_page("pages/2_阿長排班.py")
+    st.switch_page("pages/2_排班.py")
